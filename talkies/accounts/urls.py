b52cdb3f41ed9a8b8a3b5e8817d40b87abf4c602
Sourabh Jha <sourabhjha96444@gmail.com>
2022-09-29 15:28:27 +0530

    feat: mirror repo for talkies website

---
+++ talkies/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup, name="signin"),
+    path('login/', views.signin, name="signin"),
+    path('profile/', views.userprofile, name="profile"),
+    path('logout/', views.logoutuser, name="logout"),
+    path('edit/', views.userEdit, name="edit"),
+    path('deactivate/', views.deactivateUser, name="deactivate"),
+]
